9d7d6e70edd328abf2930fafad123c332252388b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-27 11:05:01 +0100

    Adds README

---
+++ ssris/README.md
@@ -0,0 +1,5 @@
+The following donut chart shows the distribution of the most commonly prescribed SSRIs (Selective Serotonin Reuptake Inhibitors), more commonly known as antidepressants, as used among women whose babies were born without birth defects.
+The chart shows that women who took Sertraline before or during early pregnancy were more likely to give birth to a baby with birth defects.
+This data comes from the National Birth Defects Prevention Study (NBDPS), one of the largest studies aimed at understanding factors that increase the risk for major birth defects in the United States.
+
+![ssris](ssris.png)
--- ssris/ssris.js
+++ ssris/ssris.js
@@ -1,11 +1,11 @@
 new Chart(document.getElementById("graph1"), {
     type: "doughnut",
     data: {
-        labels: ["Citalopram(Celexa)", "Fluoxetine(Prozac)", "Escitalopram(Lexapro", "Sertraline(Zoloft)", "Paroxetine(Paxil)"],
+        labels: ["Citalopram(Celexa)", "Fluoxetine(Prozac)", "Escitalopram(Lexapro)", "Sertraline(Zoloft)", "Paroxetine(Paxil)"],
         datasets: [
             {
               label: "Distribution of the most commonly prescribed Selective Serotonin Reuptake Inhibitors used among women whose baby did not have a birth defect",
-              backgroundColor: ["gray", "#pink","#blue","#green","black"],
+              backgroundColor: ["#ffccff", "#e6ccff", "#bf80ff", "#ccccff", "#ccffff"],
               data: [0.1, 0.26, ,0.09, 0.4, 0.15]
             }
           ]
