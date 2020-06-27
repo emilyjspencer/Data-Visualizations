5a544ef36e1c1ff224931f51b67205c8e5e646d2
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-27 05:33:32 +0100

    Changes colours

--- EthnicityOxbridge/ethnicity.js
+++ EthnicityOxbridge/ethnicity.js
@@ -4,7 +4,7 @@ new Chart(document.getElementById("graph1"), {
       labels: ["Black", "Bangladeshi or Pakistani", "Asian", "Mixed Heritage", "White"],
       datasets: [{
         label: "Ethnicity of students",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        backgroundColor: ["#b30000", "#ffff66","#ffa31a","#e8c3b9","#c45850"],
         data: [1.9, 1.7, 6.5, 6.6, 83.3]
       }]
     },
