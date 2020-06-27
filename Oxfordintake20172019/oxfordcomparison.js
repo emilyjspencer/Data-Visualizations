cc2bace76eeea85ba978e8824805e8368485b8d3
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-27 17:25:58 +0100

    Updates data - changes to use statistics from 2015 rather than 2017. Changes endpoint name

--- Oxfordintake20172019/oxfordcomparison.js
+++ Oxfordintake20172019/oxfordcomparison.js
@@ -4,20 +4,46 @@ function showData(data, labels) {
         type: 'line',
         data: {
             labels: labels,
-            datasets: [{
-                label: 'Student Intake 2017',
-                data: data,
-                backgroundColor: 'rgba(100, 134, 234, 0.2)',
-                borderColor: 'rgba(65, 121, 121, 1)',
-            }]
+            datasets: [
+                {
+                  label: 'Student Intake 2015',
+                  data: data[0],
+                  backgroundColor: 'rgba(100, 134, 234, 0.2)',
+                  borderColor: 'rgba(128, 23, 30, 1)',
+                },
+                {
+                  label: 'Student Intake 2019',
+                  data: data[1],
+                  backgroundColor: 'rgba(200, 145, 189, 0.2)',
+                  borderColor: 'rgba(159, 242, 245, 1)',
+                }
+            ]
         },
-  });
+    });
+        
 }
+
+function getData() {
+    $("#message").html('<img src="./giphy.gif" alt="" srcset="">');
+    $.ajax({
+        url: "http://localhost:3000/data",
+        success: function (result) {
+            $("#message").html("");
+            var data = [];
+            data.push(result.StudentIntake2015);
+            data.push(result.StudentIntake2019);
+            var labels = result.labels;
+            showData(data, labels);  
+        },
+        error: function (err) {
+            $("#message").html("Error");
+        }
+    });
+}
+
 
 $("#show").click(
     function () {
-        data = [ 1.9, 1.7, 6.5, 6.6, 83.3];
-        labels = ["Black", "Bangladeshi/Pakistani", "Asian", "Mixed Heritage", "White"];
-        showData(data, labels);
+      getData();
     }
 );
